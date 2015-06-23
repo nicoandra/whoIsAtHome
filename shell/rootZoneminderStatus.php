1dc0b5efc6b42496816bc2a76f331afd9114f324
Proliant Nico Andrade <nico@proliant>
2015-06-23 00:18:28 -0400

    New version of rootZoneMinder

--- shell/rootZoneminderStatus.php
+++ shell/rootZoneminderStatus.php
@@ -27,12 +27,31 @@ function handleMessages($redis, $channel, $message){
 
 
 function setNadieEnCasa(){
+	$redis = new Redis();
+	$redis->connect('127.0.0.1');
+
+	if($redis->hGet('zoneminder', 'status') == 'NadieEnCasa'){
+		echo "Status is NadieEnCasa, not changing}\n";
+		return;
+	}
+
+
 	system("/usr/bin/zmpkg.pl NadieEnCasa");
+	$redis->hset('zoneminder', 'status', 'NadieEnCasa');
 }
 
 function setPeriferia(){
+	$redis = new Redis();
+	$redis->connect('127.0.0.1');
+
+	if($redis->hGet('zoneminder', 'status') == 'Periferia'){
+		echo "Status is Periferia, not changing}\n";
+		return;
+	}
+
+
 	system("/usr/bin/zmpkg.pl Periferia");
+	$redis->hset('zoneminder', 'status', 'Periferia');
 }
 
 $redis->subscribe(array('zoneminder.status'), 'handleMessages');
-
