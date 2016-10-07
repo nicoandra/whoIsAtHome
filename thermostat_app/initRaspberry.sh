d36755ac8f89e7336fa98b8b6a66e5ced43c323a
Nicolas Andrade <nico@nmac.com.ar>
2016-10-06 20:42:59 -0400

    Adding script to init Raspberry pi

---
+++ thermostat_app/initRaspberry.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt-get update;
+sudo apt-get upgrade;
+sudo apt-get install git aptitude;
+
+
+curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+
+
