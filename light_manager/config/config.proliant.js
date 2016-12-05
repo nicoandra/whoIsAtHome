9310835748b118276dc69f20bff65a01a41cf864
Nicolas Andrade <nico@nmac.com.ar>
2016-12-05 08:26:56 -0500

    Repeat and delay for Proliant setup

--- light_manager/config/config.proliant.js
+++ light_manager/config/config.proliant.js
@@ -5,6 +5,6 @@ config.httpHost = '192.168.1.106';
 config.httpPort = '3999';
 
 
-config.milight = [{host : '192.168.1.148' , port : 8899, delayBetweenCommands : 50, repeat: 2  }];
+config.milight = [{host : '192.168.1.148' , port : 8899, delayBetweenCommands : 75, repeat: 4 }];
 
 module.exports = config;
