a2bea15a6a20fcb93d76c0811e5899ae9bd28bab
Nicolas Andrade <nico@nmac.com.ar>
2016-12-05 08:32:53 -0500

    Proliant config

--- light_manager/config/config.proliant.js
+++ light_manager/config/config.proliant.js
@@ -5,6 +5,6 @@ config.httpHost = '192.168.1.106';
 config.httpPort = '3999';
 
 
-config.milight = [{host : '192.168.1.148' , port : 8899, delayBetweenCommands : 75, repeat: 4 }];
+config.milight = [{host : '192.168.1.148' , port : 8899, delayBetweenCommands : 80, repeat: 5 }];
 
 module.exports = config;
