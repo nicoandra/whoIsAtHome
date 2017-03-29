d3267032ba75569f09bf49358c4a6c02856ad12c
Nicolas Andrade <nico@nmac.com.ar>
2017-03-28 21:36:44 -0400

    Test script for the new board

---
+++ light_manager/tests/board.js
@@ -0,0 +1,18 @@
+const dgram = require('dgram');
+
+server = dgram.createSocket('udp4');
+
+var setTempPayload = [0x10, 0x02, 0x10, 0x00];
+var getStatusPayload = [0x30, 0xFF, 0x22, 0xB8];
+
+
+var setLedPowerPayload = [0x20, 0x00, 0x03, 0xFF];
+
+server.on("message", function(message, networkInfo){
+	console.log(message);
+
+})
+
+server.bind(8888, function(){
+
+});
