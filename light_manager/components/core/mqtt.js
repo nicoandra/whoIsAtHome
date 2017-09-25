99a753ad8dd3b14c843bbbdfc41f1fb2c6e70ca6
Nico Andrade <nico@nmac.com.ar>
2017-09-25 13:54:40 -0400

    Starting with MQTT

--- light_manager/components/core/mqtt.js
+++ light_manager/components/core/mqtt.js
@@ -5,11 +5,24 @@ function messageBroker(cfg) {
 
     let client = mqtt.connect(cfg.mqtt.dsn);
 
+    function connect(){
+        client = mqtt.connect(cfg.mqtt.dsn);
+    }
+
     client.on('connect', function () {
         client.subscribe('/device/announcement')
         client.publish('/device/announcement', "Main server started");
     })
 
+    client.on('message', function (topic, message) {
+        // message is Buffer
+        console.log(message.toString())
+        client.end()
+    })
+
+    client.on('offline', function(){
+        this.connect();
+    }.bind(this))
 }
 
 module.exports = messageBroker;
